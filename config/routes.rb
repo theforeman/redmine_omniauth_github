82ad061c41b6f52976c3c9d6fa2bfd71de19bed6
Jérôme Lebleu <jerome.lebleu@mailoo.org>
2015-10-26 10:30:19 +0100

    Add Redmine 3.x compat and catch OAuth2 errors
    Based on commit 70ed292 from cy-yuto-kurosawa

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,2 @@
-get 'oauth_github', :to => 'redmine_oauth#oauth_github'
-get 'oauth2callback_github', :to => 'redmine_oauth#oauth_github_callback', :as => 'oauth_github_callback'
+get 'oauth/github', :to => 'redmine_oauth#oauth_github'
+get 'oauth/github/callback', :to => 'redmine_oauth#oauth_github_callback', :as => 'oauth_github_callback'
