3ab506217113098d33c3264f0c95c10c9ab1c8a9
Greg Sutcliffe <greg.sutcliffe@gmail.com>
2018-07-20 13:07:19 +0100

    Update Settings access from symbols to strings

--- init.rb
+++ init.rb
@@ -10,10 +10,10 @@ Redmine::Plugin.register :redmine_omniauth_github do
   author_url 'https://github.com/ares'
 
   settings :default => {
-    :client_id => "",
-    :client_secret => "",
-    :github_oauth_autentication => false,
-    :allowed_domains => "",
-    :github_self_registration => "-1"
+    'client_id' => "",
+    'client_secret' => "",
+    'github_oauth_autentication' => false,
+    'allowed_domains' => "",
+    'github_self_registration' => "-1"
   }, :partial => 'settings/github_settings'
 end
