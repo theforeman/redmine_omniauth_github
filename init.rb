1681c91136f0f531c9121bce9805ee5ac7a5cf96
Greg Sutcliffe <greg.sutcliffe@gmail.com>
2018-07-20 13:07:48 +0100

    Set minimum Redmine version for string-based access

--- init.rb
+++ init.rb
@@ -5,9 +5,10 @@ Redmine::Plugin.register :redmine_omniauth_github do
   name 'Redmine Omniauth Github plugin'
   author 'Marek Hulan'
   description 'This is a plugin for Redmine registration through github'
-  version '0.0.3'
+  version '0.0.4'
   url 'https://github.com/ares/redmine_omniauth_github'
   author_url 'https://github.com/ares'
+  requires_redmine :version_or_higher => '3.4'
 
   settings :default => {
     'client_id' => "",
